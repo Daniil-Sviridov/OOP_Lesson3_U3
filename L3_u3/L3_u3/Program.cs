17d12a751041107f8fd6919771f8c86252c9e84f
Daniil Sviridov <daniil_sviridov@mail.ru>
2022-01-24 12:23:55 +0300

    Старт

---
+++ L3_u3/L3_u3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L3_u3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileInfo fi = new FileInfo("mail.txt");
+
+            fi.CreateText();
+
+        }
+    }
+}
